52254ff17678eefe8b4ad60b93a0e4110d6c164a
Adam Tindale <adam@adamtindale.com>
2014-11-12 11:25:27 -0500

    polyphony, hooray

--- main.js
+++ main.js
@@ -3,19 +3,32 @@ window.onload = function() {
     'Hello, World! It is ' + new Date();
 };
 
-var synth = flock.synth({
+var synth = flock.synth.polyphonic({
   synthDef : {
     id : "synthy",
     ugen : "flock.ugen.sinOsc",
     freq : 200,
-    mul: 0.25,
-  }
+        mul: {
+            id: "env",
+            ugen: "flock.ugen.env.simpleASR",
+            attack: 0.25,
+            sustain: 1.0,
+            release: 0.5
+        }
+   }
+});
+
+
+
+$(document).on('keydown', function(e){
+  //console.log(e);
+  $('#greeting').html(e.which.toString());
+  synth["noteOn"]("key"+e.which.toString(), {"synthy.freq":e.which*4});
+});
+
+
+$(document).on('keyup', function(e){
+  synth["noteOff"]("key"+e.which.toString() );
 });
 
 synth.play();
-
-$(document).on('keypress', function(e){
-  console.log(e);
-  $('#greeting').html(e.toString);
-  synth.set("synthy.freq", e.which);
-});
